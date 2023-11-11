7a690284310529645de3ac5cb4184ccedfeea976
brian <90752841+wokbjso@users.noreply.github.com>
2023-11-11 19:11:31 +0900

    Refactor:footer 상태 변수로 선언 후 map으로 rendering

--- app/common/components/Footer.tsx
+++ app/common/components/Footer.tsx
@@ -6,28 +6,36 @@ import { HiDownload } from "react-icons/hi";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export const Footer = () => {
+  const FooterState = [
+    {
+      text: "Home",
+      icon: BiHomeAlt2,
+    },
+    {
+      text: "Search",
+      icon: LuSearch,
+    },
+    {
+      text: "Coming Soon",
+      icon: MdOutlineVideoLibrary,
+    },
+    {
+      text: "Downloads",
+      icon: HiDownload,
+    },
+    {
+      text: "More",
+      icon: GiHamburgerMenu,
+    },
+  ];
   return (
     <div className="fixed inset-x-0 bottom-0 flex footer-text items-center justify-center pt-2 pb-2 bg-footerBackground-main text-footerIcon-main bg-background-main">
-      <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <BiHomeAlt2 style={{ fontSize: "2.4rem" }} />
-        <div className="mt-px">Home</div>
-      </div>
-      <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <LuSearch style={{ fontSize: "2.4rem" }} />
-        <div className="mt-px">Search</div>
-      </div>
-      <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <MdOutlineVideoLibrary style={{ fontSize: "2.4rem" }} />
-        <div className="mt-px">Coming Soon</div>
-      </div>
-      <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <HiDownload style={{ fontSize: "2.4rem" }} />
-        <div className="mt-px">Downloads</div>
-      </div>
-      <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
-        <GiHamburgerMenu style={{ fontSize: "2.4rem" }} />
-        <div className="mt-px">More</div>
-      </div>
+      {FooterState.map((state) => (
+        <div className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full">
+          <state.icon style={{ fontSize: "2.4rem" }} />
+          <div className="mt-px">{state.text}</div>
+        </div>
+      ))}
     </div>
   );
 };
