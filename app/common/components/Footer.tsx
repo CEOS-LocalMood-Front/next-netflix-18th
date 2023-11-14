06efa1f855228f5d2d3289f89a03a112e7aef3a2
김지원 <jibom0507@naver.com>
2023-11-14 13:29:44 +0900

    Feat:Footer에 각 페이지 링크 연결

--- app/common/components/Footer.tsx
+++ app/common/components/Footer.tsx
@@ -8,36 +8,45 @@ import { GiHamburgerMenu } from "react-icons/gi";
 export const Footer = () => {
   const FooterState = [
     {
+      id: 0,
       text: "Home",
       icon: BiHomeAlt2,
+      link: "/main",
     },
     {
+      id: 1,
       text: "Search",
       icon: LuSearch,
+      link: "/search",
     },
     {
+      id: 2,
       text: "Coming Soon",
       icon: MdOutlineVideoLibrary,
     },
     {
+      id: 3,
       text: "Downloads",
       icon: HiDownload,
     },
     {
+      id: 4,
       text: "More",
       icon: GiHamburgerMenu,
+      link: "/main",
     },
   ];
   return (
     <div className="fixed inset-x-0 bottom-0 flex footer-text items-center justify-center pt-2 pb-2 bg-footerBackground-main text-footerIcon-main bg-background-main">
       {FooterState.map((state) => (
-        <div
-          key={state.text}
+        <a
+          href={state.link || "/"}
+          key={state.id}
           className="w-1/5 flex flex-col text-center cursor-pointer items-center h-full"
         >
           <state.icon style={{ fontSize: "2.4rem" }} />
           <div className="mt-px">{state.text}</div>
-        </div>
+        </a>
       ))}
     </div>
   );
---
+++ app/search/page.tsx
@@ -0,0 +1,7 @@
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>검색페이지</h1>
+    </div>
+  );
+}
