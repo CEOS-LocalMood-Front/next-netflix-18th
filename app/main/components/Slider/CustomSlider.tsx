2a3ed643118b9ed752e98234cb9a4c9e068924d6
brian <90752841+wokbjso@users.noreply.github.com>
2023-11-16 01:17:50 +0900

    Feat:Image 태그 blur 설정

--- app/main/components/Slider/CustomSlider.tsx
+++ app/main/components/Slider/CustomSlider.tsx
@@ -83,6 +83,8 @@ export default function CustomSlider<T>({
                 src={getMoviePoster(movie.poster_path)}
                 sizes="(max-width:768px) 33vw (max-width:1024px) 25vw, 20vw "
                 fill={true}
+                placeholder="blur"
+                blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
               />
             </div>
 
