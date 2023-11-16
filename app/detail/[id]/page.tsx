3c45a4b3bc9eafcddda11121b3895babfddc600c
brian <90752841+wokbjso@users.noreply.github.com>
2023-11-17 01:46:20 +0900

    Feat:detail 페이지 next/image 로 변경

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,6 +1,7 @@
 import getMovieDetail from "./queries/getMovieDetail";
 import { getMoviePoster } from "@/app/main/utils/get-movie-poster";
 import PlayButton from "@/app/common/components/layout/button/PlayButton";
+import Image from "next/image";
 
 export default async function DetailMovie(props: any) {
   const movieId = props.params.id;
@@ -9,12 +10,18 @@ export default async function DetailMovie(props: any) {
   console.log(movieInfo);
   return (
     <div className="bg-background-main min-h-screen">
-      <img
-        className="w-full h-[41.5rem] "
-        alt="영화 이미지"
-        src={getMoviePoster(movieInfo.poster_path)}
-      />
-      <div className="absolute top-0 w-full h-[42rem] bg-gradient-to-t from-black via-transparent to-transparent"></div>
+      <div className="w-full h-[41.5rem] relative">
+        <Image
+          alt="영화 이미지"
+          src={getMoviePoster(movieInfo.poster_path)}
+          sizes="100vw"
+          fill={true}
+          placeholder="blur"
+          blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+          priority
+        />
+      </div>
+      <div className="absolute top-0 w-full h-[42rem] bg-gradient-to-t from-black via-transparent to-transparent" />
       <div className="px-[3.6rem] flex justify-center mt-[1.3rem]">
         <PlayButton className="w-[30.3rem] h-[4.5rem] " />
       </div>
