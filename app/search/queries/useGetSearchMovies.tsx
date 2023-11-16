3a7202e70e09bfe248c0d6d1bd65d2509125d94c
김지원 <jibom0507@naver.com>
2023-11-16 12:54:08 +0900

    Chore:useGetSearchMovies 타입 지정

--- app/search/queries/useGetSearchMovies.tsx
+++ app/search/queries/useGetSearchMovies.tsx
@@ -2,19 +2,35 @@ import { searchAxiosInstance } from "@/app/common/libs/axios";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { IMovie } from "@/app/main/queries/dto/get-popular-movie";
 
+interface DateRange {
+  maximum: string;
+  minimum: string;
+}
+
+interface SearchMovieResponse {
+  dates: DateRange;
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export async function getSearchMovies({
   searchText,
   pageParam,
 }: {
   searchText: string;
   pageParam: number;
-}) {
-  const response = await searchAxiosInstance.get<any>("/search/movie", {
-    params: {
-      query: searchText,
-      page: pageParam,
-    },
-  });
+}): Promise<SearchMovieResponse> {
+  const response = await searchAxiosInstance.get<SearchMovieResponse>(
+    "/search/movie",
+    {
+      params: {
+        query: searchText,
+        page: pageParam,
+      },
+    }
+  );
   const searchFetchedMovies = response.data;
   return searchFetchedMovies;
 }
