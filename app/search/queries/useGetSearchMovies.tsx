63e204a9cae7ef361750f64e290c7f8e9192843b
김지원 <jibom0507@naver.com>
2023-11-17 01:52:27 +0900

    Feat:실시간 검색으로 변경

--- app/search/queries/useGetSearchMovies.tsx
+++ app/search/queries/useGetSearchMovies.tsx
@@ -1,12 +1,10 @@
 import { searchAxiosInstance } from "@/app/common/libs/axios";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { IMovie } from "@/app/main/queries/dto/get-popular-movie";
 
 interface DateRange {
   maximum: string;
   minimum: string;
 }
-
 interface SearchMovieResponse {
   dates: DateRange;
   page: number;
@@ -14,7 +12,6 @@ interface SearchMovieResponse {
   total_pages: number;
   total_results: number;
 }
-
 export async function getSearchMovies({
   searchText,
   pageParam,
@@ -35,28 +32,4 @@ export async function getSearchMovies({
   return searchFetchedMovies;
 }
 
-export default function useGetSearchMovies({
-  searchText,
-}: {
-  searchText: string;
-}) {
-  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery({
-    queryKey: ["getSearchMovie"],
-    queryFn: ({ pageParam = 1 }) =>
-      getSearchMovies({
-        searchText,
-        pageParam,
-      }),
-    getNextPageParam: (lastPage) =>
-      lastPage.page !== lastPage.total_pages ? lastPage.page + 1 : undefined,
-  });
-
-  const rawMovieData = data?.pages.map((page) => page.results).flat() || [];
-  const getByFarMovieData: IMovie[] = rawMovieData;
-
-  return {
-    getByFarMovieData,
-    hasNextPage,
-    fetchNextPage,
-  };
-}
+export default getSearchMovies;
